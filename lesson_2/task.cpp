6dac4ee2f30c396a9223fc61f0ac18aa8ecad135
Nikolos123 <Isponec2>
2021-12-27 23:33:15 +0300

    lesson_2

--- lesson_2/task.cpp
+++ lesson_2/task.cpp
@@ -18,9 +18,13 @@ lesson 2
 using namespace  std;
 void task1();
 void task2();
+void task3();
+void task4();
 
 int main(){
     task1();
+    task2();
+    task3();
     return 0;
 }
 
@@ -53,16 +57,60 @@ void task1(){
 
     cout << "bool false_true " << false_true  << "\n";
     cout << "char ascii " << one_ascii  << "\n";
-    cout << "short int  max and min " << max_short_int  << " - " <<min_short_int << "\n";
-    cout << "int  max and min " << max_int  << " - " <<min_int << "\n";
-    cout << "long long   max and min " << long_long_max  << " - " <<long_long_min << "\n";
+    cout << "short int  max and min " << max_short_int  << " / " <<min_short_int << "\n";
+    cout << "int  max and min " << max_int  << " / " <<min_int << "\n";
+    cout << "long long   max and min " << long_long_max  << " / " <<long_long_min << "\n";
     cout << "float num_float " << num_float  << "\n";
-    cout << "double num_doble " << num_doble  << "\n";
+    cout << "double num_double " << num_doble  << "\n";
 
 
 
 }
 
 void task2(){
+
+    enum  class Tic_Tac_Toe{X, O};
+
+    Tic_Tac_Toe ZERO = Tic_Tac_Toe::O;
+    Tic_Tac_Toe CROSS = Tic_Tac_Toe::X;
+
+    if (ZERO == Tic_Tac_Toe::O){
+        cout << "The is Zero!\n";
+    }else{
+        cout << "The is not Zero!\n";
+    }
+
+    if (CROSS == Tic_Tac_Toe::X){
+        cout << "The is CROSS!\n";
+    }else{
+        cout << "The is not CROSS!\n";
+    }
+}
+
+void task3(){
+
+    enum   Tic_Tac_Toe{X, O};
+
+    Tic_Tac_Toe symbol_zero[1] = {X,};
+    Tic_Tac_Toe symbol_cross[1] = {O};
+
+    //Выводим в консоль
+    cout << "zero" << symbol_zero[0] << endl; //очищаем поток и переносим на новую строку
+    cout << "cross" << symbol_cross[0] << endl; //очищаем поток и переносим на новую строку
+
+    //Меняем местами значения
+    symbol_zero[0] = X;
+    symbol_cross[0] = O;
+
+    //Выводим в консоль
+    cout << "cross" << symbol_zero[0] << endl; //очищаем поток и переносим на новую строку
+    cout << "zero" << symbol_cross[0] << endl; //очищаем поток и переносим на новую строку
+
+
+}
+
+void task4(){
+
+    
 
 }
