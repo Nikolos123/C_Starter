e5503fc46b773111aa6262b22d0f20ccafb7bfdd
Nikolos123 <Isponec2>
2022-02-28 23:55:22 +0300

    lesson_5

---
+++ lesson_5/task.cpp
@@ -0,0 +1,34 @@
+/*Nagorniy Nikolay
+
+lesson 5
+В одном tack.cpp файле / проекте:
+
+
+    1.Написать программу, проверяющую что сумма двух (введенных с клавиатуры)
+    чисел лежит в пределах от 10 до 20 (включительно),
+    если да – вывести строку "true", в противном случае – "false";
+
+
+
+*/
+
+#include <iostream>
+#include <vector>
+
+
+void task1();
+
+
+int main() {
+
+    task1();
+
+    return 0;
+}
+
+void task1() {
+
+        std::cout << "False " << std::endl;
+
+}
+
